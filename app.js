e41acb9f508ff0b3e5572987894cf85292ef7fa8
Lucas8901 <lucas25011989@gmail.com>
2023-01-15 17:05:45 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var btn = document.getElementById('btn');
+var btn1 = document.getElementById('btn1');
+var btn2 = document.getElementById('btn2');
+var btn3 = document.getElementById('btn3');
+
+function Toggle(){
+    if(btn.style.color == 'red'){
+      btn.style.color ='grey'
+    }
+    else{
+      btn.style.color = 'red'
+    }
+}
+
+function Toggle1(){
+  if(btn1.style.color == 'red'){
+    btn1.style.color ='grey'
+  }
+  else{
+    btn1.style.color = 'red'
+  }
+}
+
+function Toggle2(){
+  if(btn2.style.color == 'red'){
+    btn2.style.color ='grey'
+  }
+  else{
+    btn2.style.color = 'red'
+  }
+}
+
+function Toggle3(){
+  if(btn3.style.color == 'red'){
+    btn3.style.color ='grey'
+  }
+  else{
+    btn3.style.color = 'red'
+  }
+}
